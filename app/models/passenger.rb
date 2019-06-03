cfc3a82dc03fd41c29178a7814802da17098e97c
github username <github email address>
2019-06-03 10:12:03 -0400

    Done - Ganesh Raj

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,46 @@
+class Passenger
+
+    attr_reader :name
+
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        Passenger.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def rides
+        Ride.all.select do |ride|
+            ride.passenger == self
+        end
+    end
+
+    def drivers
+        self.rides.collect do |ride|
+            ride.driver
+        end
+    end
+
+    def total_distance
+        total_distance = 0
+        self.rides.each do |ride|
+            total_distance = total_distance + ride.distance
+        end
+        total_distance
+    end
+
+    def self.premium_members
+        self.all.select do |member|
+            member.total_distance > 100
+        end
+    end
+    
+
+
+end
+
 
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,30 @@
+class Ride
 
+    attr_accessor :driver, :passenger, :distance
+
+    @@all = []
+
+    def initialize(driver, passenger, distance)
+        @driver = driver
+        @passenger = passenger
+        @distance = distance
+
+        Ride.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.average_distance
+        sum = 0
+        count = 0
+        self.all.each do |ride|
+            sum = sum + ride.distance
+            count += 1
+        end
+        sum / count
+    end
+
+
+end
