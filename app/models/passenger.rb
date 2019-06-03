bab24d135d46485881f9b9d036880a27e7d7d29f
Matthew Peoples <matthew.peoples@yale.edu>
2019-06-03 10:29:52 -0400

    time up

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,44 @@
+class Passenger 
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def rides
+        Ride.all.select do |find|
+            find.passenger == self
+        end
+    end
+
+    def drivers
+        Ride.all.select do |find|
+            find.passenger == self
+        end.map do |drive|
+            drive.driver
+        end
+    end
+
+    def total_distance
+        dist_arr = Ride.all.select do |find|
+            find.passenger == self
+        end.map do |drive|
+            drive.distance
+        end
+        tot_dist = dist_arr.inject(0){|sum,x| sum + x }
+    end
+
+    def self.premium_members
+
+        self.all.select do |premium|
+            premium.total_distance > 100.0
+        end
+    end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -1,4 +1,7 @@
 require_relative '../config/environment.rb'
+require_relative '../app/models/driver.rb'
+require_relative '../app/models/passenger.rb'
+require_relative '../app/models/ride.rb'
 
 def reload
   load 'config/environment.rb'
@@ -6,5 +9,25 @@ end
 
 
 # Put your variables here~!
+phill = Driver.new("Phill")
+brenda = Driver.new("Brenda")
+jacob = Driver.new("Jacob")
+gigi = Driver.new("Gigi")
 
+
+tommy = Passenger.new("Tommy")
+jack = Passenger.new("Jack")
+
+ride1 = Ride.new(phill, tommy, 3.4)
+ride3 = Ride.new(jacob, tommy, 7.2)
+ride4 = Ride.new(gigi, tommy, 99.9)
+ride5 = Ride.new(gigi, jack, 61.2)
+ride2 = Ride.new(brenda, jack, 6.2)
+
+tommy.rides
+tom_drivers = tommy.drivers
+tommy.total_distance
+gigi.passenger_names
+
+Passenger
 binding.pry
