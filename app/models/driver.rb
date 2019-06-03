d2fe31998024ce218f98a5f0bea253b44e4e47c9
leoniedong <leoniedong@hotmail.com>
2019-06-03 10:01:54 -0400

    done

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,37 @@
+class Driver
+
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def passenger_names
+        Ride.all.select do |ride|
+            ride.driver == self
+        end.map do |ride|
+            ride.passenger.name
+        end
+    end
+
+    def rides
+        Ride.all.select do |ride|
+            ride.driver == self
+        end
+    end
+
+    def self.mileage_cap(distance)
+        Ride.all.select do |ride|
+            ride.distance >= distance
+        end.map do |ride|
+            ride.driver
+        end.uniq
+    end
+end
 
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,38 @@
+class Passenger
+    attr_reader :name
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    
+    def rides
+        Ride.all.select do |ride|
+            ride.passenger == self
+        end
+    end
+
+    def drivers
+        self.rides.map {|ride| ride.driver}
+    end
+
+    def total_distance
+        sum = 0
+        self.rides.each do |ride|
+            sum += ride.distance
+        end
+        sum
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.premium_members
+        self.all.select do |passenger|
+            passenger.total_distance >= 100
+        end
+    end
+end
 
