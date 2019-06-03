1ddc0e117d6bdd0cb186285cc671adee3ca9dff9
rinakawamura <rina.kawamura@yale.edu>
2019-06-03 10:24:43 -0400

    Done.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,37 @@
+class Driver
+    attr_reader :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def rides
+        Ride.all.select do |ride|
+            ride.driver == self
+        end
+    end
+     
+    def passenger_names
+        self.rides.map do |ride|
+            ride.passenger.name
+        end.uniq
+    end
+
+    def self.all
+        @@all
+    end
+
+    def total_distance
+        self.rides.map do |ride|
+            ride.distance
+        end.sum 
+    end
+
+    def self.mileage_cap(distance)
+        self.all.select do |driver|
+            driver.total_distance > distance
+        end
+    end
+end
