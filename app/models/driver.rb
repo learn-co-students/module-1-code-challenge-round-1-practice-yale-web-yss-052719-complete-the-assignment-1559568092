c863e59052b7bd50d82829d7781942b9d3520866
XRuiz9 <xdruiz92@gmail.com>
2019-06-03 10:06:56 -0400

    done

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,40 @@
+class Driver
+	attr_accessor :name
+	@@all = []
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+	def rides
+		Ride.all.select do |ride|
+			ride.driver == self
+		end
+	end
+
+	def passenger_names
+		self.rides.map do |ride|
+			ride.passenger
+		end
+	end
+
+	def total_mileage
+		total = 0
+		self.rides.each do |ride|
+			total += ride.distance
+		end
+		total
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.mileage_cap(distance)
+		self.all.select do |driver|
+			driver.total_mileage > distance.to_f
+		end
+	end
+end
+
 
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,23 @@
+class Ride
+	attr_reader :passenger, :driver, :distance
+	@@all = []
+	def initialize(passenger, driver, distance)
+		@passenger = passenger
+		@driver = driver
+		@distance = distance.to_f
+		@@all << self
+	end
 
+	def self.all
+		@@all
+	end
+
+	def self.average_distance
+		total = 0
+		self.all.each do |ride|
+			total += ride.distance 
+		end
+		total/self.all.count
+	end
+
+end
