ffc83308fe284582145952aa5b87574627a7a240
jqzheng617 <jenna.zheng617@gmail.com>
2019-06-03 10:26:07 -0400

    done

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,32 @@
+class Passenger
 
+  attr_reader :name
+  @@all = []
+
+  def initialize(name, rides, drivers, total_distance)
+    @name = name
+    @rides = rides
+    @drivers = drivers
+    @total_distance = total_distance
+    @@all << self
+  end
+
+  def rides
+    Ride.new.all
+  end
+
+  def drivers
+    Driver.new.all
+  end
+
+  def total_distance
+    
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.premium_members
+  end
+end
